24e8c81dc196f35de34629c495ca8096c5410934
arafat-cse <arafat.tufan15@gmail.com>
2024-11-20 23:21:47 +0600

    Table add Start

---
+++ SmartCourier/Models/Bank.cs
@@ -0,0 +1,17 @@
+﻿namespace SmartCourier.Models
+{
+    public class Bank
+    {
+        public int BankId { get; set; }
+        public int CompanyID { get; set; }
+        public string? Address { get; set; }
+        public string? AccountNo { get; set; }
+        public string? BranchName { get; set; }
+        public int? CreateBy { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public DateTime UpdateBy { get; set; }
+        public DateTime? UpdateDate { get; set; }
+        public bool? IsActive { get; set; }
+
+    }
+}
---
+++ SmartCourier/Models/Company.cs
@@ -0,0 +1,11 @@
+﻿namespace SmartCourier.Models
+{
+    public class Company
+    {
+        public int CompanyId { get; set; }
+        public string? CompanyName { get; set; }
+        public int? CreateBy { get; set; }
+        public int? CreateDate { get; set; }
+
+    }
+}
