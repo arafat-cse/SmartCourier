405a09f68205c4e398b4dca92d567bcd8b144270
Abdur-Rahman-1 <abdurrahmanhriday22@gmail.com>
2024-11-28 00:44:11 +0600

    Driver Column Add

---
+++ SmartCourier/Models/Driver.cs
@@ -0,0 +1,9 @@
+﻿namespace SmartCourier.Models
+{
+    public class Driver
+    {
+        public int DriverId { get; set; }
+        public string? DriverName { get; set; }
+        public string? PhoneNumber { get; set; }
+    }
+}
