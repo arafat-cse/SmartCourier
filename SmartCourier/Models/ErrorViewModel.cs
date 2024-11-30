cfb450877adbc3f0560fffbe1e91a20e8fcf499e
arafat-cse <arafat.tufan15@gmail.com>
2024-11-30 20:51:13 +0600

    DbContext Add

--- SmartCourier/Models/ErrorViewModel.cs
+++ SmartCourier/Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace SmartCourier.Models
 {
     public class ErrorViewModel
     {
-        public string? equestId { get; set; }
+        public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
